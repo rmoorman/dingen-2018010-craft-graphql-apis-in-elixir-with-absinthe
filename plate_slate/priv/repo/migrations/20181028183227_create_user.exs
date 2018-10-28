dab98ea728f21cc09211456b15f69948a98b5c86
Rico Moorman <rico.moorman@gmail.com>
2018-10-28 19:33:57 +0100

    Add generator output for user schema
    mix phx.gen.schema Accounts.User user name:string email:string password:string role:string

---
+++ plate_slate/priv/repo/migrations/20181028183227_create_user.exs
@@ -0,0 +1,15 @@
+defmodule PlateSlate.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:user) do
+      add :name, :string
+      add :email, :string
+      add :password, :string
+      add :role, :string
+
+      timestamps()
+    end
+
+  end
+end
