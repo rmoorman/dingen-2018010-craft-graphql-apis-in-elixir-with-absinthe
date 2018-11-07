59d1a4aa58b0348151d8c8ec3c6bf1f0c45b3891
Rico Moorman <rico.moorman@gmail.com>
2018-11-07 14:08:58 +0100

    Connect the login handling to the login mutation and add a logout button

--- plate_slate/lib/plate_slate_web/controllers/session_controller.ex
+++ plate_slate/lib/plate_slate_web/controllers/session_controller.ex
@@ -6,4 +6,30 @@ defmodule PlateSlateWeb.SessionController do
   def new(conn, _) do
     render(conn, "new.html")
   end
+
+  @graphql """
+  mutation ($email: String!, $password: String!) @action(mode: INTERNAL) {
+    login(role: EMPLOYEE, email: $email, password: $password)
+  }
+  """
+  def create(conn, %{data: %{login: result}}) do
+    case result do
+      %{user: user, token: _token} ->
+        conn
+        |> put_session(:employee_id, user.id)
+        |> put_flash(:info, "Login successful")
+        |> redirect(to: Routes.item_path(conn, :index))
+
+      _ ->
+        conn
+        |> put_flash(:info, "Wrong email or password")
+        |> render("new.html")
+    end
+  end
+
+  def delete(conn, _) do
+    conn
+    |> clear_session()
+    |> redirect(to: Routes.session_path(conn, :new))
+  end
 end
