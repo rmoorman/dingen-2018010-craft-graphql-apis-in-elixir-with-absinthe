e418d3b5c1dd0c1ea270d9dff56f79b4ff9ad803
Rico Moorman <rico.moorman@gmail.com>
2018-11-15 02:30:52 +0100

    Stub out the listing

--- plate-slate-apollo-ui/src/App.js
+++ plate-slate-apollo-ui/src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  get menuItems() {
+    return [
+      {id: "stub-1", name: "Stub Menu Item 1"},
+      {id: "stub-2", name: "Stub Menu Item 2"},
+      {id: "stub-3", name: "Stub Menu Item 3"},
+    ]
+  }
+
+  renderMenuItem(menuItem) {
+    return (
+      <li key={menuItem.id}>{menuItem.name}</li>
+    )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <ul>
+        {this.menuItems.map(this.renderMenuItem)}
+      </ul>
+    )
   }
 }
 
